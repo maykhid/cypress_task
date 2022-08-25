76770a70f2f14c5bf226cd1bc9a6cac7120af62f
Henry Ifebunandu <henryifebunandu@gmail.com>
2022-08-25 21:38:22 +0100

    cache setup

--- lib/features/album/data/data_sources/local/album_local.dart
+++ lib/features/album/data/data_sources/local/album_local.dart
@@ -1,11 +1,33 @@
-import 'package:cypress_task/features/album/data/data_sources/album_data_src.dart';
 import 'package:cypress_task/features/album/data/model/response/album_response.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class AlbumLocalDataSrcImpl implements AlbumDataSrc {
+import '../../../../../core/data/exception.dart';
+
+abstract class AlbumLocalDataSrc {
+  Future<List<AlbumResponse>>? getCachedAlbums();
+  Future<void> cacheAlbum(List<AlbumResponse> response);
+}
+
+const albumStorageKey = '___ALBUM__KEY___';
+
+class AlbumLocalDataSrcImpl implements AlbumLocalDataSrc {
+  final SharedPreferences prefs;
+
+  AlbumLocalDataSrcImpl({required this.prefs});
+
   @override
-  Future<List<AlbumResponse>>? getAlbums() {
-    // TODO: implement getAlbum
-    throw UnimplementedError();
+  Future<List<AlbumResponse>>? getCachedAlbums() {
+    final jsonResponse = prefs.getString(albumStorageKey);
+
+    if (jsonResponse != null) {
+      return Future.value(albumResponseFromMap(jsonResponse));
+    } else {
+      throw CacheException();
+    }
   }
-  
+
+  @override
+  Future<void> cacheAlbum(List<AlbumResponse> response) {
+    return prefs.setString(albumStorageKey, albumResponseToMap(response));
+  }
 }
--- lib/features/album/data/data_sources/local/photo_local.dart
+++ lib/features/album/data/data_sources/local/photo_local.dart
@@ -1,13 +1,32 @@
-import 'package:cypress_task/features/album/data/data_sources/remote/picture_data_src.dart';
+import 'package:cypress_task/core/data/exception.dart';
 import 'package:cypress_task/features/album/data/model/response/photo_response.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../photo_data_src.dart';
+abstract class PhotoLocalDataSrc {
+  Future<List<PhotoResponse>>? getCachedPhotos();
+  Future<void> cachePhotos(List<PhotoResponse> response);
+}
 
-class PhotoLocalDataSrcImpl implements PhotoDataSrc{
+const photoStorageKey = '___PHOTO__KEY___';
+
+class PhotoLocalDataSrcImpl implements PhotoLocalDataSrc {
+  final SharedPreferences prefs;
+
+  PhotoLocalDataSrcImpl({required this.prefs});
+
   @override
-  Future<List<PhotoResponse>>? getPhotos() {
-    // TODO: implement getPhotos
-    throw UnimplementedError();
+  Future<List<PhotoResponse>>? getCachedPhotos() {
+    final jsonResponse = prefs.getString(photoStorageKey);
+
+    if (jsonResponse != null) {
+      return Future.value(photoResponseFromMap(jsonResponse));
+    } else {
+      throw CacheException();
+    }
   }
 
+  @override
+  Future<void> cachePhotos(List<PhotoResponse> response) {
+    return prefs.setString(photoStorageKey, photoResponseToMap(response));
+  }
 }
